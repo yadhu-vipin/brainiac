e2dd8503099981381aa0c3ef9d022c0e05381b52
unknown <yadhuvipin12@gmail.com>
2025-03-08 22:44:59 +0530

    update: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
-
-{
-  "rules"; {
-    "@typescript-eslint/no-unused-vars"; "off"
-  }
-}
-
 
 export default eslintConfig;
